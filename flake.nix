a8681ced6bafbc08ab70956ca43c21279db2edcf
phaedrusflow <map@qompass.ai>
2025-06-26 13:18:05 -0700

    Update metadata, badge, and citation for 2025-06-26

--- flake.nix
+++ flake.nix
@@ -3,33 +3,67 @@
 # Copyright (C) 2025 Qompass AI, All rights reserved
 
 {
-  description = "Qompass AI TuxType on NixOS";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [
-        pkgs.autoconf
-        pkgs.automake
-        pkgs.libtool
-        pkgs.pkg-config
-        pkgs.gcc
-        pkgs.SDL
-        pkgs.SDL_image
-        pkgs.SDL_mixer
-        pkgs.SDL_ttf
-        pkgs.gettext
-        pkgs.gtk2
-      ];
-      shellHook = ''
-        echo "Ready to build TuxType!"
-        echo "Run: ./autogen.sh && ./configure && make"
-      '';
-    };
+  description = "QompassAI on TuxType";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # Native builds for Linux/macOS/Windows
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ 
+            "aarch64-linux-android" 
+            "x86_64-linux-android"
+          ];
+        };
+        tuxtype = pkgs.rustPlatform.buildRustPackage {
+          pname = "tuxtype";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = [ rustToolchain ];
+          CARGO_BUILD_TARGET = if pkgs.stdenv.hostPlatform.isWindows
+            then "x86_64-pc-windows-gnu"
+            else null;
+        };
+      in {
+        packages.default = tuxtype;
+        apps.default = flake-utils.lib.mkApp { drv = tuxtype; };
+      }
+    ) // {
+      packages = let
+        androidSystems = {
+          aarch64-android = "aarch64-unknown-linux-android";
+          x86_64-android = "x86_64-unknown-linux-android";
+        };
+        mkAndroidPackage = androidSystem: androidTarget:
+          let
+            pkgs = import nixpkgs {
+              system = "x86_64-linux";
+              crossSystem = {
+                config = androidTarget;
+                useAndroidPrebuilt = true;
+              };
+            };
+          in
+            pkgs.rustPlatform.buildRustPackage {
+              pname = "tuxtype";
+              version = "0.1.0";
+              src = ./.;
+              cargoLock.lockFile = ./Cargo.lock;
+              nativeBuildInputs = [ pkgs.androidndk ];
+              ANDROID_NDK_ROOT = "${pkgs.androidndk}/libexec/android-sdk/ndk-bundle";
+            };
+      in
+        builtins.mapAttrs
+          (name: target: { default = mkAndroidPackage name target; })
+          androidSystems;
+    };
 }
-
